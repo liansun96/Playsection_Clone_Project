67a5d15a7dfc8e803d7a2cb14d0a16d22d13d30c
liansun96 <toetetaung3296@gmail.com>
2022-08-30 20:30:40 +0630

    updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,81 @@
+const leftIcon = `<img src="../images/left-arrow.svg" class="nav-img" alt="left">`;
+const rightIcon = `<img src="../images/right-arrow.svg" class="nav-img" alt="right">`;
+
+
+const swiper1 = new Swiper('.swiper1', {
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+//   Optional parameters
+// direction: 'vertical',
+    loop: true,
+    effect: "fade",
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
+const swiper3 = new Swiper('.swiper3', {
+    loop: true,
+    // effect: "fade",
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+    },
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+
 var owlMiniCard = $('#owl-miniCard');
 owlMiniCard.owlCarousel({
     margin:17,
@@ -144,33 +222,35 @@ owlTwoMobile.owlCarousel({
     }
 });
 
-var owlTwo = $('#owl-two');
-owlTwo.owlCarousel({
-    margin:21,
-    loop:true,
-    autoWidth:true,
-    nav: true,
-    dots :true,
-    navText: [ "< PREV  " , "  NEXT >" ],
-    responsiveClass:true,
-    // autoplay:true,
-    autoplayTimeout:2000,
-    // autoplayHoverPause:true,
-    delay : 3000,
-    dotsEach: true,
+$(document).ready(function(){
+    var owlTwo = $('#owl-two');
+    owlTwo.owlCarousel({
+        margin:21,
+        loop:true,
+        autoWidth:true,
+        nav: true,
+        dots :true,
+        navText: ["<i class='fa fa-long-arrow-left'></i>","<i class='fa fa-long-arrow-right'></i>"],
+        responsiveClass:true,
+        // autoplay:true,
+        autoplayTimeout:2000,
+        // autoplayHoverPause:true,
+        delay : 3000,
+        dotsEach: true,
 
-    responsive:{
-        0:{
-            items:2
-        },
-        600:{
-            items:3
-        },
-        960:{
-            items:5
-        },
-        1200:{
-            items:6
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            960:{
+                items:5
+            },
+            1200:{
+                items:6
+            }
         }
-    }
+    });
 });
